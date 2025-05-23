43a67d32b66b3e70b49122bcc5b6d59ca8fb0eb0
ZAVDFER <ZAV.Andrey.Yar@gmail.com>
2025-05-23 11:48:36 +0300

    com

--- src/main/java/net/zavdfer/micmod/MICMod.java
+++ src/main/java/net/zavdfer/micmod/MICMod.java
@@ -1,15 +1,21 @@
 package net.zavdfer.micmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.zavdfer.micmod.item.modItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -21,6 +27,14 @@ public class MICMod {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public MICMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+
+        modItems.register(modEventBus);
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
 
     }
 
@@ -38,6 +52,9 @@ public class MICMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+        event.accept(modItems.metallurgical_plant);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
